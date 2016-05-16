482a96f951753610f0cbe5eed4a4389af3205111
Meven Moser <Meven Moser>
2016-05-16 20:57:03 +0200

    Ajout de la documentation de Bluetooth_com.dll

---
+++ Rendu/Rapport technique/Dll_Bluetooth/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bgm",
+  1: "b",
+  2: "gm"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Tout",
+  1: "Fichiers",
+  2: "Fonctions"
+};
+
