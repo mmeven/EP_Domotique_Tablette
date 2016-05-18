6febc316f6af291dc9551c1ba6fc8124068f842c
Meven Moser <Meven Moser>
2016-05-18 20:36:31 +0200

    Affichages dans les appels vers RequeteHttp

--- ModelDll/ModelDll/src/Equipment.cpp
+++ ModelDll/ModelDll/src/Equipment.cpp
@@ -83,7 +83,7 @@ namespace EP {
 		char* c1((char *) s1.c_str());
 		char* c2((char *) s2.c_str());
 
-		cout << c1 << " ; " << c2 << endl;
+		cerr << c1 << " ; " << c2 << endl;
 
 		requeteHttpKira(c1, c2);
 		return 0;
@@ -132,7 +132,7 @@ namespace EP {
 		action += m_equipmentId;
 		action += s6 + m_action[300];
 
-		cout << getIpFibaro() << " ; " << action << " ; " << Fibaro_login << " ; " << Fibaro_password << endl;
+		cerr << getIpFibaro() << " ; " << action << " ; " << Fibaro_login << " ; " << Fibaro_password << endl;
 
 		requeteHttpFibaro(getIpFibaro(), action, Fibaro_login, Fibaro_password);
 
